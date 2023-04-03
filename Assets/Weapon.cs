2687b06543f9a2c94967460f96b697a6f49c654f
mrsagcan <mrsagcan@yahoo.com>
2023-04-03 03:02:42 +0300

    Receiving raycast hit from weapon

---
+++ Assets/Weapon.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private Camera FPCamera;
+    [SerializeField] private float range = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
+        Debug.Log("You mf hit the " + hit.transform.name + "!!!");
+    }
+}
