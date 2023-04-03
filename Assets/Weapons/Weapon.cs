14dbe17d9692b9a7657803eceb86f1c27ee4b240
mrsagcan <mrsagcan@yahoo.com>
2023-04-03 05:32:39 +0300

    Hit points have impact effects now (imported the whole standard assets to get rid of future problems)

--- Assets/Weapons/Weapon.cs
+++ Assets/Weapons/Weapon.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
@@ -8,6 +9,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] private float range = 100f;
     [SerializeField] private float weaponDamage;
     [SerializeField] private ParticleSystem muzzleFlash;
+    [SerializeField] private GameObject hitEffect;
 
     // Update is called once per frame
     void Update()
@@ -34,10 +36,16 @@ public class Weapon : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
-            Debug.Log("You mf hit the " + hit.transform.name + "!!!");
+            CreateHitImpact(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
             if (target == null) return;
             target.TakeDamage(weaponDamage);
         }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact,0.1f);
     }
 }
